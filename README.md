592a4f571c20e0e20da5efcbe661be136bf29e2d
rifleh700 <rifleh700@protonmail.ch>
2025-10-12 12:46:12 +0400

    change event + readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# ps2_filter
+MTA:SA PS2 color filter shader
+
+
+![screenshot2](https://i.imgur.com/IFl1RiM.jpeg)
+![screenshot3](https://i.imgur.com/TEjSbhF.png)
+![screenshot1](https://i.imgur.com/eNwxn7r.jpeg)
+![screenshot4](https://i.imgur.com/uSyTJSA.png)
--- ps2f_main_client.lua
+++ ps2f_main_client.lua
@@ -1,18 +1,23 @@
 local SCREEN_WIDTH, SCREEN_HEIGHT = guiGetScreenSize()
 local SHADER = dxCreateShader("ps2.fx")
 
-addEventHandler("onClientPreRender", root,
+addEventHandler("onClientHUDRender", root,
 	function()
 
-		-- We do it here since other resources can reset filter sometimes
+		if getCameraGoggleEffect() ~= "normal" then
+			resetColorFilter()
+			return
+		end
+
 		setColorFilter(0, 0, 0, 0, 0, 0, 0, 0)
 
 		local r1, g1, b1, a1, r2, g2, b2, a2 = getColorFilter(true)
 		dxSetShaderValue(SHADER, "color1", r1, g1, b1, a1)
 		dxSetShaderValue(SHADER, "color2", r2, g2, b2, a2)
 		dxDrawImage(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SHADER)
-	end
-	-- We should use "high" priority here, but we can't since then graphical bug appears when u press F12
+	end,
+	false,
+	"high"
 )
 
 addEventHandler("onClientResourceStop", resourceRoot,
